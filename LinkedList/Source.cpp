a322a4f08360879251d465084086b8c4aab67347
Vinicius <me@viniciusals.com>
2020-05-26 19:17:01 -0700

    Test popFront

--- LinkedList/Source.cpp
+++ LinkedList/Source.cpp
@@ -18,7 +18,68 @@ int main(void) {
 	myList.addBack(8);
 	myList.addFront(9);
 
+	cout << myList.getSize() << endl;
 	myList.printList();
+	cout << endl;
+
+	cout << "poped val: " << myList.popFront();
+	cout << " size: " << myList.getSize() << " list:";
+	myList.printList();
+	cout << endl;
+
+	cout << "poped val: " << myList.popFront();
+	cout << " size: " << myList.getSize() << " list:";
+	myList.printList();
+	cout << endl;
+
+	cout << "poped val: " << myList.popFront();
+	cout << " size: " << myList.getSize() << " list:";
+	myList.printList();
+	cout << endl;
+
+	cout << "poped val: " << myList.popFront();
+	cout << " size: " << myList.getSize() << " list:";
+	myList.printList();
+	cout << endl;
+
+	cout << "poped val: " << myList.popFront();
+	cout << " size: " << myList.getSize() << " list:";
+	myList.printList();
+	cout << endl;
+
+	cout << "poped val: " << myList.popFront();
+	cout << " size: " << myList.getSize() << " list:";
+	myList.printList();
+	cout << endl;
+
+	cout << "poped val: " << myList.popFront();
+	cout << " size: " << myList.getSize() << " list:";
+	myList.printList();
+	cout << endl;
+
+	cout << "poped val: " << myList.popFront();
+	cout << " size: " << myList.getSize() << " list:";
+	myList.printList();
+	cout << endl;
+
+	cout << "poped val: " << myList.popFront();
+	cout << " size: " << myList.getSize() << " list:";
+	myList.printList();
+	cout << endl;
+
+	try {
+
+		cout << "poped val: " << myList.popFront();
+		cout << " size: " << myList.getSize() << " list:";
+		myList.printList();
+		cout << endl;
+	}
+
+	catch (const std::logic_error& e) {
+		
+		cout << "Sorry kiddo :(" << endl;
+	}
+
 
 	return 0;
 }
