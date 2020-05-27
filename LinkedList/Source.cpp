cba21f9ec2f1e22e65d8aeb7bb53fa5a764a726d
Vinicius <me@viniciusals.com>
2020-05-26 18:33:03 -0700

    Test addFront and addBack methods

--- LinkedList/Source.cpp
+++ LinkedList/Source.cpp
@@ -8,7 +8,17 @@ int main(void) {
 
 	LinkedList<int> myList;
 
-	cout << "test" << endl;
+	myList.addFront(1);
+	myList.addBack(2);
+	myList.addFront(3);
+	myList.addBack(4);
+	myList.addFront(5);
+	myList.addBack(6);
+	myList.addFront(7);
+	myList.addBack(8);
+	myList.addFront(9);
+
+	myList.printList();
 
 	return 0;
 }
